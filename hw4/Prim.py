d013b5e6e280e95420ee6e4eaf8919ecb4f33826
Andrew Berg <andrewberg@pobox.com>
2016-11-17 16:33:34 -0500

    Initial Commit of Kruskal, fixed Prim ordering. Added BuildPairs so that I can sort based on first vertex and then second if they are equal. Added comments. Testing works for most test files. Added total weight at the top.

--- hw4/Prim.py
+++ hw4/Prim.py
@@ -29,23 +29,40 @@ for edge in edges:
 
 def findLowest(choose, notIncluded):
 	min = sys.maxint # min value to maxint
-	min_index = 0 # min index to 0
+	index = 0 # min index to 0
 
 	for v in range(nodeCount):
-		if (notIncluded[v] == False and choose[v] < min):
-			min = choose[v]
-			min_index = v
-	return min_index
+		if (notIncluded[v] == False and choose[v] < min): # check if visited
+			min = choose[v] # and if this choice is going to be less
+			index = v # sets the index = to the found v
+	return index
+
+def buildPairs(conMST, g):
+	iterMST = iter(conMST) # skips first element
+	next(iterMST) # skip first element
+	x = 1 # starts at x =1
+	y = []
+	for i in iterMST:
+		y.append((conMST[x]+1, x+1)) # appends the tuples to y
+		x += 1
+	y = sorted(y, key=lambda a: (a[0], a[1])) # sorts on a[0] then a[1]
+	return y
 
 def printMST(conMST, g):
 	iterMST = iter(conMST)
 	next(iterMST) # skip first element
-	print(len(conMST) - 1) # print size of MST
 
+	totalCostMST = 0
 	x = 1
 	for i in iterMST:
-		print "%d %d %d" % (conMST[x]+1, x+1, graph[x][conMST[x]])
+		totalCostMST += graph[x][conMST[x]]
 		x += 1
+
+	print(totalCostMST) # print size of MST
+	pairs = buildPairs(conMST, g) # build pairs based on conMST and g
+
+	for i in pairs:
+		print "%d %d" % (i[0], i[1]) # print each pair tup1 then tup2
 
 conMST = [0 for x in range(nodeCount)] # init all values to 0
 choose = [sys.maxint for x in range(nodeCount)] # init all values to 0
@@ -64,5 +81,6 @@ for vCount in range(nodeCount): # iterate over all the vertices
 		if (graph[u][v] and notIncluded[v] == False and graph[u][v] < choose[v]):
 			conMST[v] = u # find the lowest and set it equal to mst
 			choose[v] = graph[u][v] # set the lowest weight for this spot
+
 
 printMST(conMST, graph)
