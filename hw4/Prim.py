08697ab3f513c7c16cef87730308bf86feaabbfa
Andrew Berg <andrewberg@pobox.com>
2016-11-16 22:46:51 -0500

    Added Test1 and Added Input Processing Maps each input into a matrix that is like [nodeCount][nodeCount] = weight for that actual edge

--- hw4/Prim.py
+++ hw4/Prim.py
@@ -1,4 +1,31 @@
 #!/usr/bin/python
 
+from sys import argv
+
 # Andrew Berg Prim.py for COP4531 due 11/22/16
 
+# open file that is passed in argv[1]
+
+file = open(argv[1], "r") # open file named in argv[1] in read mode
+
+# grab number of nodes
+
+nodeCount = int(file.readline().strip()) # read in number of nodes
+
+# nodecount by nodecount graph storage
+graph = [[0 for x in range(nodeCount)] for y in range(nodeCount)]
+
+# read by line and then parse line into lists
+edges = [] # empty list
+for line in file:
+	edges.append(line.strip().split(" ")) #strips \n then splits on " "
+
+# populate edges in graph, must subtract 1 from each index
+for edge in edges:
+	node1 = int(edge[0]) # first node of the edge converted to int
+	node2 = int(edge[1]) # second node of the edge converted to int
+	weight = int(edge[2]) # weight of the edge converted to int
+
+	graph[node1-1][node2-1] = weight # weight of the edge added to graph
+
+print(graph)
