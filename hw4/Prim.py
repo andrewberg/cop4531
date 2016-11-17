674b8b0e3424fd772413b7a9c5f7d3e87ad68366
Andrew Berg <andrewberg@pobox.com>
2016-11-17 11:00:24 -0500

    Prim.py Possibly Working Needs Testing Works on T2 input, needs to be tested with more testfiles.

--- hw4/Prim.py
+++ hw4/Prim.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python
 
+import sys
 from sys import argv
-
 # Andrew Berg Prim.py for COP4531 due 11/22/16
-
 # open file that is passed in argv[1]
 
 file = open(argv[1], "r") # open file named in argv[1] in read mode
 
 # grab number of nodes
-
 nodeCount = int(file.readline().strip()) # read in number of nodes
 
 # nodecount by nodecount graph storage
@@ -28,4 +26,47 @@ for edge in edges:
 
 	graph[node1-1][node2-1] = weight # weight of the edge added to graph
 
-print(graph)
+def findLowest(choose, notIncluded):
+	min = sys.maxint # min value to maxint
+	min_index = 0 # min index to 0
+
+	for v in range(nodeCount):
+		if (notIncluded[v] == False and choose[v] < min):
+			min = choose[v]
+			min_index = v
+	return min_index
+
+def printMST(conMST, g):
+	iterMST = iter(conMST)
+	next(iterMST) # skip first element
+	print(len(conMST) - 1) # print size of MST
+
+	x = 1
+	for i in iterMST:
+		print "%d %d %d" % (conMST[x], x, graph[x][conMST[x]])
+		x += 1
+
+conMST = [0 for x in range(nodeCount)] # init all values to 0
+choose = [sys.maxint for x in range(nodeCount)] # init all values to 0
+notIncluded = [False] * nodeCount # init all vertices to not visited
+
+conMST[0] = -1 # set the root of the MST
+choose[0] = 0 # make 0 so this is the first vertex being chosen
+
+for vCount in range(nodeCount): # iterate over all the vertices
+	# pick the minimum choose vertex from the set of verts not in MST
+	u = findLowest(choose, notIncluded)
+	
+	notIncluded[u] = True # add the picked vertex to the notIncluded
+
+	for v in range(nodeCount):
+		if (graph[u][v] and notIncluded[v] == False and graph[u][v] < choose[v]):
+			conMST[v] = u
+			choose[v] = graph[u][v]
+
+printMST(conMST, graph)
+
+
+
+
+
